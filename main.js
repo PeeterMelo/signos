c04d0675b0c07bbf61959e8d882c77d7d031b1b7
PeeterMelo <115720708+PeeterMelo@users.noreply.github.com>
2023-04-12 17:07:53 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const toggleBtn = document.getElementById('toggle-mode')
+const body = document.body 
+
+
+
+toggleBtn.addEventListener('click', function() {
+    document.body.classList.toggle/*click e nao click*/('dark-mode')
+   
+
+})
